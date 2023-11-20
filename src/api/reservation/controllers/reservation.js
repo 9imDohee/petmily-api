f0ea8bfc6be6d2d41a123bf7c32ee0388765d832
김도희 <doheekim@gimdohuis-MacBook-Pro.local>
2023-11-20 15:52:37 +0900

    펫밀리 서버

--- src/api/reservation/controllers/reservation.js
+++ src/api/reservation/controllers/reservation.js
@@ -12,236 +12,8 @@ const { createCoreController } = require("@strapi/strapi").factories;
 module.exports = createCoreController(
   "api::reservation.reservation",
   ({ strapi }) => ({
-    // 예약 1개 조회
-    async findOne(ctx) {
-      try {
-        const { id } = ctx.params;
-        const reservation = await strapi.entityService.findOne(
-          "api::reservation.reservation",
-          id,
-          {
-            populate: {
-              client: {
-                populate: {
-                  photo: true,
-                },
-              },
-              petsitter: {
-                populate: {
-                  photo: true,
-                },
-              },
-              pets: {
-                populate: {
-                  file: true,
-                },
-              },
-              review: true,
-              journal: true,
-            },
-          }
-        );
-        const petNames = reservation.pets.map((pet) => pet.name);
-        const petPhotos = reservation.pets.map((pet) => pet.file);
-
-        console.log(reservation);
-        const response = {
-          reservationId: reservation.id,
-          reservationDate: reservation.reservationDate,
-          reservationTimeStart: reservation.reservationTimeStart,
-          reservationTimeEnd: reservation.reservationTimeEnd,
-          address: reservation.address,
-          phone: reservation.petsitter.phone,
-          body: reservation.body,
-          progress: reservation.progress,
-          member: {
-            memberId: reservation.client.id,
-            name: reservation.client.username,
-            nickName: reservation.client.nickName,
-            body: reservation.client.body,
-            photo:
-              reservation.client.photo &&
-              `http://localhost:1337${reservation.client.photo.url}`,
-          },
-          petsitter: {
-            petsitterId: reservation.petsitter.id,
-            name: reservation.petsitter.username,
-            nickName: reservation.petsitter.nickName,
-            body: reservation.petsitter.body,
-            photo:
-              reservation.petsitter.photo && reservation.petsitter.photo.url,
-          },
-          pets: reservation.pets,
-          reviewId: reservation.review ? reservation.review.id : null,
-          journalId: reservation.journal.id,
-        };
-
-        delete response.client;
-
-        ctx.send(response);
-      } catch (e) {
-        console.log(e);
-      }
-    },
-
-    //펫시터 예약 확정
-    async confirmReservation(ctx) {
-      if (!ctx.state.user) {
-        ctx.send("에러");
-      } else {
-        try {
-          const userId = ctx.state.user.id;
-          console.log(userId);
-
-          const reservationId = +ctx.params.reservationId;
-          console.log(reservationId);
-
-          const reservation = await strapi.entityService.findOne(
-            "api::reservation.reservation",
-            reservationId,
-            {
-              populate: {
-                petsitter: true,
-              },
-            }
-          );
-          console.log(reservation);
-
-          if (
-            userId === reservation.petsitter.id &&
-            reservation.progress === "RESERVATION_REQUEST"
-          ) {
-            try {
-              // console.log(ctx.request.body);
-
-              const response = await strapi.entityService.update(
-                "api::reservation.reservation",
-                reservationId,
-                { data: { ...reservation, progress: "RESERVATION_CONFIRMED" } }
-              );
-
-              ctx.send("ok");
-            } catch (e) {
-              console.log(e);
-            }
-          }
-        } catch (e) {
-          console.log(e);
-        }
-      }
-    },
-
-    // 펫시터 예약 취소
-    async petsitterCancel(ctx) {
-      if (!ctx.state.user) {
-        ctx.send("에러");
-      } else {
-        try {
-          const userId = ctx.state.user.id;
-
-          const reservationId = +ctx.params.reservationId;
-
-          const reservation = await strapi.entityService.findOne(
-            "api::reservation.reservation",
-            reservationId,
-            {
-              populate: {
-                petsitter: true,
-              },
-            }
-          );
-
-          if (
-            userId === reservation.petsitter.id &&
-            reservation.progress === "RESERVATION_CONFIRMED"
-          ) {
-            try {
-              const response = await strapi.entityService.update(
-                "api::reservation.reservation",
-                reservationId,
-                { data: { ...reservation, progress: "RESERVATION_CANCELLED" } }
-              );
-
-              ctx.send("cancelled");
-            } catch (e) {}
-          }
-        } catch (e) {}
-      }
-    },
-
-    // 멤버 예약 취소
-    async memberCancel(ctx) {
-      if (!ctx.state.user) {
-        ctx.send("에러");
-      } else {
-        try {
-          const userId = ctx.state.user.id;
-
-          const reservationId = +ctx.params.reservationId;
-          console.log(reservationId);
-
-          const reservation = await strapi.entityService.findOne(
-            "api::reservation.reservation",
-            reservationId,
-            {
-              populate: {
-                client: true,
-              },
-            }
-          );
-
-          if (
-            userId === reservation.client.id &&
-            reservation.progress === "RESERVATION_REQUEST"
-          ) {
-            try {
-              const response = await strapi.entityService.update(
-                "api::reservation.reservation",
-                reservationId,
-                { data: { ...reservation, progress: "RESERVATION_CANCELLED" } }
-              );
-              ctx.send("cancelled");
-            } catch (e) {
-              console.log(e);
-            }
-          }
-        } catch (e) {
-          console.log(e);
-        }
-      }
-    },
-
-    async create(ctx) {
-      // 예약 생성
-      try {
-        const data = JSON.parse(ctx.request.body.data);
-        const reservation = await strapi.entityService.create(
-          "api::reservation.reservation",
-          {
-            data: {
-              reservationBody: data.body,
-              reservationDate: data.reservationDate,
-              reservationTimeStart: data.reservationTimeStart,
-              reservationTimeEnd: data.reservationTimeEnd,
-              address: data.address,
-              phone: data.phone,
-              pets: data.petId,
-              petsitter: data.petsitterId,
-              client: ctx.state.user.id,
-              progress: "RESERVATION_REQUEST",
-            },
-          }
-        );
-
-        return (ctx.body = "Create Reservation Success");
-      } catch (e) {
-        console.error(e);
-        return ctx.badRequest("Reservation 생성 실패");
-      }
-    },
-
+    // 멤버, 펫시터 예약 조회
     async find(ctx) {
-      // 멤버, 펫시터 예약 조회
       const { id: userId } = ctx.state.user;
       const { type } = ctx.state.user.role;
 
@@ -408,100 +180,310 @@ module.exports = createCoreController(
       }
     },
 
-    async findPossiblePetsitter(ctx) {
-      // 예약정보에 맞는 펫시터 조회
-      const {
-        reservationDate,
-        reservationTimeStart,
-        reservationTimeEnd,
-        address,
-        petId,
-      } = ctx.request.body;
+    // 예약 1개 조회
+    async findOne(ctx) {
+      if (!ctx.state.user) {
+        ctx.send("에러");
+      } else {
+        try {
+          const userId = ctx.state.user.id;
+          const { id } = ctx.params;
 
-      // petsitter검색
-      try {
-        const pets = await strapi.entityService.findMany("api::pet.pet", {
-          filters: {
-            id: petId,
-          },
-        });
+          const reservation = await strapi.entityService.findOne(
+            "api::reservation.reservation",
+            id,
+            {
+              populate: {
+                client: {
+                  fields: ["username", "nickName", "body"],
+                  populate: {
+                    photo: true,
+                  },
+                },
+                petsitter: {
+                  fields: ["username", "nickName", "phone", "body"],
+                  populate: {
+                    photo: true,
+                  },
+                },
+                pets: {
+                  fields: [
+                    "type",
+                    "name",
+                    "age",
+                    "species",
+                    "weight",
+                    "male",
+                    "neutering",
+                    "body",
+                    ,
+                  ],
+                  populate: {
+                    photo: true,
+                  },
+                },
+                review: { fields: ["id"] },
+                journal: { fields: ["id"] },
+              },
+            }
+          );
 
-        const petsitters = await strapi.entityService.findMany(
-          "plugin::users-permissions.user",
-          {
-            filters: {
-              possibleDay: {
-                $contains: new Date(reservationDate).toLocaleDateString(
-                  "ko-KR",
-                  {
-                    weekday: "short",
-                  }
-                ),
+          if (
+            userId === reservation.client.id ||
+            userId === reservation.petsitter.id
+          ) {
+            const response = {
+              reservationId: reservation.id,
+              reservationDate: reservation.reservationDate,
+              reservationTimeStart: reservation.reservationTimeStart,
+              reservationTimeEnd: reservation.reservationTimeEnd,
+              address: reservation.address,
+              phone: reservation.petsitter.phone,
+              body: reservation.body,
+              progress: reservation.progress,
+              member: {
+                memberId: reservation.client.id,
+                name: reservation.client.username,
+                nickName: reservation.client.nickName,
+                body: reservation.client.body,
+                photo:
+                  reservation.client.photo &&
+                  reservation.client.photo.formats.thumbnail.url,
               },
-              possibleTimeStart: {
-                $lte: reservationTimeStart,
+              petsitter: {
+                petsitterId: reservation.petsitter.id,
+                name: reservation.petsitter.username,
+                nickName: reservation.petsitter.nickName,
+                body: reservation.petsitter.body,
+                photo:
+                  reservation.petsitter.photo &&
+                  reservation.petsitter.photo.formats.thumbnail.url,
               },
-              possibleTimeEnd: {
-                $gte: reservationTimeEnd,
-              },
-              possibleLocation: {
-                $contains: address.split(" ").slice(1, 3).join(" "),
-              },
-              possiblePetType: {
-                $contains: pets.map((pet) => pet.type),
-              },
-            },
-            populate: { photo: { fields: ["url"] } },
+              pets: reservation.pets,
+              reviewId: reservation.review && reservation.review.id,
+              journalId: reservation.journal && reservation.journal.id,
+            };
+
+            ctx.send(response);
           }
-        );
+        } catch (e) {
+          console.log(e);
+        }
+      }
+    },
 
-        const possiblePetsittersId = petsitters.map(
-          (petsitter) => petsitter.id
-        );
-        console.log(possiblePetsittersId);
+    // 예약 생성
+    async create(ctx) {
+      if (!ctx.state.user) {
+        ctx.badRequest("로그인을 해주세요");
+      } else {
+        try {
+          const data = ctx.request.body;
 
-        const reviews = await strapi.entityService.findMany(
-          "api::review.review",
-          {
-            populate: {
-              reservation: {
-                populate: {
-                  petsitter: { filters: { id: possiblePetsittersId } },
+          const reservation = await strapi.entityService.create(
+            "api::reservation.reservation",
+            {
+              data: {
+                ...data,
+                pets: data.petId,
+                petsitter: data.petsitterId,
+                client: ctx.state.user.id,
+                progress: "RESERVATION_REQUEST",
+              },
+            }
+          );
+
+          return (ctx.body = "Create Reservation Success");
+        } catch (e) {
+          console.error(e);
+          return ctx.badRequest("Reservation 생성 실패");
+        }
+      }
+    },
+
+    //펫시터 예약 확정
+    async confirmReservation(ctx) {
+      if (!ctx.state.user) {
+        ctx.send("에러");
+      } else {
+        try {
+          const userId = ctx.state.user.id;
+          const reservationId = +ctx.params.reservationId;
+
+          const reservation = await strapi.entityService.findOne(
+            "api::reservation.reservation",
+            reservationId,
+            {
+              populate: {
+                petsitter: { fields: ["id"] },
+              },
+            }
+          );
+
+          if (
+            userId === reservation.petsitter.id &&
+            reservation.progress === "RESERVATION_REQUEST"
+          ) {
+            try {
+              const response = await strapi.entityService.update(
+                "api::reservation.reservation",
+                reservationId,
+                { data: { progress: "RESERVATION_CONFIRMED" } }
+              );
+
+              ctx.send("ok");
+            } catch (e) {
+              ctx.badRequest("예약 확정에 실패하였습니다.");
+              console.log(e);
+            }
+          }
+        } catch (e) {
+          console.log(e);
+        }
+      }
+    },
+
+    // 펫시터 예약 취소
+    async petsitterCancel(ctx) {
+      if (!ctx.state.user) {
+        ctx.send("에러");
+      } else {
+        try {
+          const userId = ctx.state.user.id;
+          const reservationId = +ctx.params.reservationId;
+
+          const reservation = await strapi.entityService.findOne(
+            "api::reservation.reservation",
+            reservationId,
+            {
+              populate: {
+                petsitter: { fields: ["id"] },
+              },
+            }
+          );
+
+          console.log(reservation);
+
+          if (
+            userId === reservation.petsitter.id &&
+            reservation.progress === "RESERVATION_CONFIRMED"
+          ) {
+            try {
+              const response = await strapi.entityService.update(
+                "api::reservation.reservation",
+                reservationId,
+                { data: { progress: "RESERVATION_CANCELLED" } }
+              );
+
+              ctx.send("cancelled");
+            } catch (e) {
+              ctx.badRequest("예약 취소에 실패했습니다.");
+            }
+          }
+        } catch (e) {}
+      }
+    },
+
+    // 멤버 예약 취소
+    async memberCancel(ctx) {
+      if (!ctx.state.user) {
+        ctx.send("에러");
+      } else {
+        try {
+          const userId = ctx.state.user.id;
+          const reservationId = +ctx.params.reservationId;
+
+          const reservation = await strapi.entityService.findOne(
+            "api::reservation.reservation",
+            reservationId,
+            {
+              populate: {
+                client: { fields: ["id"] },
+              },
+            }
+          );
+
+          if (
+            userId === reservation.client.id &&
+            reservation.progress === "RESERVATION_REQUEST"
+          ) {
+            try {
+              const response = await strapi.entityService.update(
+                "api::reservation.reservation",
+                reservationId,
+                { data: { ...reservation, progress: "RESERVATION_CANCELLED" } }
+              );
+              ctx.send("cancelled");
+            } catch (e) {
+              console.log(e);
+              ctx.badRequest("예약 취소에 실패했습니다.");
+            }
+          }
+        } catch (e) {
+          console.log(e);
+        }
+      }
+    },
+
+    // 예약정보에 맞는 펫시터 조회
+    async findPossiblePetsitter(ctx) {
+      if (!ctx.state.user) {
+        ctx.badRequest("로그인이 필요합니다.");
+      } else if (ctx.state.user.role.type === "public") {
+        const {
+          reservationDate,
+          reservationTimeStart,
+          reservationTimeEnd,
+          address,
+          petId,
+        } = ctx.request.body;
+
+        // petsitter검색
+        try {
+          const pets = await strapi.entityService.findMany("api::pet.pet", {
+            filters: {
+              id: petId,
+            },
+          });
+
+          const petsitters = await strapi.entityService.findMany(
+            "plugin::users-permissions.user",
+            {
+              filters: {
+                possibleDay: {
+                  $contains: new Date(reservationDate).toLocaleDateString(
+                    "ko-KR",
+                    { weekday: "short" }
+                  ),
+                },
+                possibleTimeStart: {
+                  $lte: reservationTimeStart,
+                },
+                possibleTimeEnd: { $gte: reservationTimeEnd },
+                possibleLocation: {
+                  $contains: address.split(" ").slice(1, 3).join(" "),
+                },
+                possiblePetType: {
+                  $contains: pets.map((pet) => pet.type),
                 },
               },
-            },
-          }
-        );
-        console.log(reviews);
+            }
+          );
 
-        // console.log(petsitters);
-        const possiblePetsitters = petsitters.map((petsitter) => ({
-          petsitterId: petsitter.id,
-          name: petsitter.username,
-          nickName: petsitter.nickName,
-          photo: petsitter.photo ? petsitter.photo.url : null,
-          possibleDay: petsitter.possibleDay ? petsitter.possibleDay : null,
-          possibleTimeStart: petsitter.possibleTimeStart,
-          possibleTimeEnd: petsitter.possibleTimeEnd,
-          star: petsitter.star ? petsitter.star : null,
-          reviewCount: petsitter.reviewCount ? petsitter.reviewCount : null,
-        }));
-
-        // ctx.send(possiblePetsitters);
-      } catch (e) {
-        console.error("해당 예약에 맞는 펫시터를 찾을 수 없습니다.");
+          ctx.send(petsitters);
+        } catch (e) {}
       }
     },
 
     // 펫시터 예약일정 조회
     async sitterSchedule(ctx) {
-      const { id: petsitterId } = ctx.params;
+      const { petsitterId } = ctx.params;
 
-      console.log(petsitterId);
       try {
         const reservations = await strapi.entityService.findMany(
           "api::reservation.reservation",
           {
+            sort: { reservationDate: "asc" },
             filters: {
               petsitter: { id: { $eq: petsitterId } },
               reservationDate: {
@@ -514,8 +496,6 @@ module.exports = createCoreController(
           }
         );
 
-        console.log(reservations);
-
         const transformedReservations = reservations.map((reservation) => ({
           reservationId: reservation.id,
           reservationDate: reservation.reservationDate,
@@ -524,7 +504,6 @@ module.exports = createCoreController(
           progress: reservation.progress,
         }));
 
-        console.log(transformedReservations);
         ctx.send(transformedReservations);
       } catch (error) {
         console.error(error);
